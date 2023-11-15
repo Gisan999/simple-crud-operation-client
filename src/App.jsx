d54db50d405be403d6003674d1e9abab7b68a9c8
Gisan999 <ruheatheking3@gmail.com>
2023-11-16 00:48:19 +0600

    my first crud operation code is here

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+
+// import { json } from 'react-router-dom';
+import './App.css'
+
+function App() {
+
+  const handleAddUsers = event =>{
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = {name, email} ;
+    console.log(user);
+
+    fetch('http://localhost:5000/users', {
+      method:'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      if(data.insertedId){
+        alert('Users added successfully')
+        form.reset();
+      }
+    })
+  }
+
+  return (
+    <>
+      <h1>simple CRUD</h1>
+      <div className="relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+        <div className="relative mx-4 -mt-6 mb-4 grid h-28 place-items-center overflow-hidden rounded-xl bg-gradient-to-tr from-pink-600 to-pink-400 bg-clip-border text-white shadow-lg shadow-pink-500/40">
+          <h3 className="block font-sans text-3xl font-semibold leading-snug tracking-normal text-white antialiased">
+            Sign In
+          </h3>
+        </div>
+        <form onSubmit={handleAddUsers}>
+          <div className="flex flex-col gap-4 p-6">
+            <div className="relative h-11 w-full min-w-[200px]">
+              <input
+                className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                type='text'
+                name='name'
+              />
+             
+            </div>
+            <div className="relative h-11 w-full min-w-[200px]">
+              <input
+                className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                type='email'
+                name='email'
+              />
+            </div>
+
+          </div>
+          <div className="p-6 pt-0">
+            <button
+              className="block w-full select-none rounded-lg bg-gradient-to-tr from-pink-600 to-pink-400 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              type="submit"
+              data-ripple-light="true"
+            >
+              Sign In
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  )
+}
+
+export default App
